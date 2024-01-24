e2fd6ad368b673c8a49355f311a9b95c3eae4ec6
jake-knox- <jakeknox11@hotmail.co.uk>
2024-01-24 10:38:22 +0000

    adding empty components, basic styles from last app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+// <Switch> component has been deprecated and replaced with the <Routes> component.
+// <Route exact path="/" component={Home} /> 
+// https://www.w3schools.com/react/react_router.asp
 
 import './App.scss';
 
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Dashboard from './components/Dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+        <Navbar />
 
-    </div>
+        <Routes>
+
+          <Route exact path="/" Component={HomePage}></Route>
+          <Route exact path="/dashboard" Component={Dashboard}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+// I like the look of the bbc navbar
+// https://www.bbc.co.uk/
+// note - the list items are hidden from right->left as screen size decreses. News is last.
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img
+                src='./bookends-logo-tr.png'
+            />
+            <ul className='navbar'>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
