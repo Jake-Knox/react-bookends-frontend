da1576eda2f17eeb7435a76e4dad5f0ad2a49167
jake-knox- <jakeknox11@hotmail.co.uk>
2024-01-25 11:16:02 +0000

    work on user lists, links, styles testing

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -15,6 +15,7 @@ const UserList = () => {
                     <li
                         key={user.id}
                     >
+                        <img className='avatar' src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light' />
                         <Link to={`/user/${user.name}`}>{user.name}</Link>
                     </li>
                 ))}
